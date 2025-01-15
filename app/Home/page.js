d6e21cb8651671bee1f370903e80b2f018ed0e6f
Pranjal1804 <mitrapranjal2004@gmailcom>
2025-01-16 00:15:51 +0530

    Building Home Page

---
+++ app/Home/page.js
@@ -0,0 +1,22 @@
+import Header from '@/Components/Header'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <Header />
+    <div id='intro'>
+      <div id="text">
+        <p>Lorem ipsum dolor</p>
+        <p>sit amet, consectetur adipiscing</p>
+        <p>prgrt paro fres elit.</p>
+        </div>
+        <div id="image">
+        <img src="/7971fd50-0fc9-48af-b66c-58de5921f02b.jpeg" alt='picture' id='pic1'/>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Home
