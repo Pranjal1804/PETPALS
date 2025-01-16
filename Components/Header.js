53a3038a00bc6756b260ad2a6033f3d6ed5e01b1
Pranjal1804 <mitrapranjal2004@gmailcom>
2025-01-16 20:10:59 +0530

    Updated Design

--- Components/Header.js
+++ Components/Header.js
@@ -1,23 +1,24 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 const Header = () => {
   return (
-    <div id='header'>
-        <div >
-            <img src='/ebf7d701-f85f-49b0-b84a-d60ea2036984.png' id='logo'/>
-        </div>
-        <div>
-            <nav id='navbar'>
-                <a href=''>Home</a>
-                <a href=''>Browse Pet</a>
-                <a href=''>Dashboard</a>
-                <a href=''>Services</a>
-                <a href=''>About</a>
-                <a href=''>Contact</a>
-            </nav>
-        </div>
+    <div id="header">
+      <div>
+        <img src="/ebf7d701-f85f-49b0-b84a-d60ea2036984.png" id="logo" alt="Logo" />
+      </div>
+      <div>
+        <nav id="navbar">
+          <Link href="/Home">Home</Link>
+          <Link href="/browse-pet">Browse Pet</Link>
+          <Link href="/dashboard">Dashboard</Link>
+          <Link href="/services">Services</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+        </nav>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
